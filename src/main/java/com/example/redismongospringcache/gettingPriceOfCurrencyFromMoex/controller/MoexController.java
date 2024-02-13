52719f76d2b7e250a73e38f9cb0de008de4ae7da
dasdassand <dasdassand@gmail.com>
2024-02-13 10:49:45 +0300

    fix

--- src/main/java/com/example/redismongospringcache/gettingPriceOfCurrencyFromMoex/controller/MoexController.java
+++ src/main/java/com/example/redismongospringcache/gettingPriceOfCurrencyFromMoex/controller/MoexController.java
@@ -2,28 +2,30 @@ package com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.control
 
 import com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.dto.CurrencyPriceMaxAndMinDto;
 import com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.service.MoexService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Controller for processing requests to obtain the maximum and minimum price of a currency
  */
 @RestController
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MoexController {
-    @Autowired
-    private  final MoexService moexService;
+
+    private final MoexService moexService;
+
     /**
      * Obtaining the maximum and minimum price of a currency by its code.
+     *
      * @param currencyCod currency code.
      * @return a CurrencyPriceMaxAndMinDto object with information about the maximum and minimum price.
      */
     @GetMapping("/moex/{currencyCod}")
-    public CurrencyPriceMaxAndMinDto getPriceCurrency(@PathVariable String currencyCod){
+    public CurrencyPriceMaxAndMinDto getPriceCurrency(@PathVariable String currencyCod) {
         log.info("GetMapping request '/getPriceCurrency' get list  Currency price max and min");
         return moexService.getCurrencyMinAndMaxPriceInfo(currencyCod);
     }
--- src/main/java/com/example/redismongospringcache/repository/CurrencyRateDBRepository.java
+++ src/main/java/com/example/redismongospringcache/repository/CurrencyRateDBRepository.java
@@ -1,7 +1,6 @@
 package com.example.redismongospringcache.repository;
 
 import com.example.redismongospringcache.model.CurrencyRate;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface CurrencyRateDBRepository extends MongoRepository<CurrencyRate, String> {
     CurrencyRate findByName(String name);
 
-    void deleteById(@NotNull String id);
+    void deleteById(String id);
 }
