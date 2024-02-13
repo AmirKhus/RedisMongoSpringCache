be6e3f428326102cf14e95c55be93ad6ff804590
AmirKhus <amir19040268@gmail.com>
2024-02-13 10:47:24 +0300

    добавлена дока

--- src/main/java/com/example/redismongospringcache/gettingPriceOfCurrencyFromMoex/controller/MoexController.java
+++ src/main/java/com/example/redismongospringcache/gettingPriceOfCurrencyFromMoex/controller/MoexController.java
@@ -1,23 +1,30 @@
 package com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.controller;
 
-import com.example.redismongospringcache.exception.ExceptionService;
 import com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.dto.CurrencyPriceMaxAndMinDto;
 import com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.service.MoexService;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * Controller for processing requests to obtain the maximum and minimum price of a currency
+ */
 @RestController
-@RequiredArgsConstructor
+@Slf4j
+@AllArgsConstructor
 public class MoexController {
-
-    private final MoexService moexService;
-    private final ExceptionService exceptionService;
-
+    @Autowired
+    private  final MoexService moexService;
+    /**
+     * Obtaining the maximum and minimum price of a currency by its code.
+     * @param currencyCod currency code.
+     * @return a CurrencyPriceMaxAndMinDto object with information about the maximum and minimum price.
+     */
     @GetMapping("/moex/{currencyCod}")
-    public CurrencyPriceMaxAndMinDto getPriceCurrency(@PathVariable String currencyCod) {
-        assert currencyCod != null : exceptionService.illegalArgumentException("currencyCod is null from getPriceCurrency()");
+    public CurrencyPriceMaxAndMinDto getPriceCurrency(@PathVariable String currencyCod){
+        log.info("GetMapping request '/getPriceCurrency' get list  Currency price max and min");
         return moexService.getCurrencyMinAndMaxPriceInfo(currencyCod);
     }
 }
