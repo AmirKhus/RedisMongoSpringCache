811adcfd8c35ce2dfcf3e2cd51257fb1bee8eb35
AmirKhus <amir19040268@gmail.com>
2024-02-13 10:12:19 +0300

    добавлена документация

--- src/main/java/com/example/redismongospringcache/gettingPriceOfCurrencyFromMoex/controller/MoexController.java
+++ src/main/java/com/example/redismongospringcache/gettingPriceOfCurrencyFromMoex/controller/MoexController.java
@@ -3,18 +3,26 @@ package com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.control
 import com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.dto.CurrencyPriceMaxAndMinDto;
 import com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.service.MoexService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * Controller for processing requests to obtain the maximum and minimum price of a currency
+ */
 @RestController
+@Slf4j
 @AllArgsConstructor
 public class MoexController {
-    @Autowired
-    private  final MoexService moexService;
+    private final MoexService moexService;
+    /**
+     * Obtaining the maximum and minimum price of a currency by its code.
+     * @param currencyCod currency code.
+     * @return a CurrencyPriceMaxAndMinDto object with information about the maximum and minimum price.
+     */
     @GetMapping("/moex/{currencyCod}")
-    public CurrencyPriceMaxAndMinDto getPriceCurrency(@PathVariable String currencyCod){
+    public CurrencyPriceMaxAndMinDto getPriceCurrency(@PathVariable String currencyCod) {
+        log.info("GetMapping request '/getPriceCurrency' get list  Currency price max and min");
         return moexService.getCurrencyMinAndMaxPriceInfo(currencyCod);
     }
 }
--- src/main/java/com/example/redismongospringcache/gettingPriceOfCurrencyFromMoex/service/MoexServiceImpl.java
+++ src/main/java/com/example/redismongospringcache/gettingPriceOfCurrencyFromMoex/service/MoexServiceImpl.java
@@ -10,12 +10,21 @@ import org.springframework.web.client.RestTemplate;
 
 import static com.example.redismongospringcache.gettingPriceOfCurrencyFromMoex.dto.MarketData.getMoexDtoToCurrencyDto;
 
+/**
+* Implementation of a service for obtaining the maximum and minimum prices of currencies from the MOEX exchange.
+ */
 @Service
 public class MoexServiceImpl implements MoexService {
     @Autowired
     private RestTemplate restTemplate;
     @Value("${parse.moex.url}")
     private String moexParseUrl;
+/**
+     * Gets the maximum and minimum prices of a currency from the MOEX exchange by its code.
+     *
+     * @param currencyCod currency code
+     * @return a CurrencyPriceMaxAndMinDto object with the maximum and minimum prices of the currency
+     */
     @Override
     public CurrencyPriceMaxAndMinDto getCurrencyMinAndMaxPriceInfo(String currencyCod) {
         CurrencySecID currencySecID = CurrencySecID.valueOf(currencyCod);
